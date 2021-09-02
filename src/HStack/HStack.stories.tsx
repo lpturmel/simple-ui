f0c19ffb42d96a956da6548caf689a59c0fa84ec
Louis-Philippe Turmel <turmlo@2020spaces.com>
2021-09-01 23:10:40 -0400

    Updated HStack Story

--- src/HStack/HStack.stories.tsx
+++ src/HStack/HStack.stories.tsx
@@ -2,17 +2,24 @@ import Button from "../Button/Button";
 import HStack from "./HStack";
 
 export default {
-    component: HStack,
-    title: "UI/HStack",
+	component: HStack,
+	title: "UI/HStack",
 };
 
 export const Default = () => (
-    <HStack>
-        <Button px={4} py={2}>
-            Button
-        </Button>
-        <Button px={4} py={2}>
-            Button
-        </Button>
-    </HStack>
+	<HStack>
+		<Button
+			_hover={{ bg: "blue.500" }}
+			_active={{ bg: "blue.600" }}
+			bg={"blue.400"}
+			color="black"
+			px={4}
+			py={2}
+		>
+			Button
+		</Button>
+		<Button px={4} py={2}>
+			Button
+		</Button>
+	</HStack>
 );
