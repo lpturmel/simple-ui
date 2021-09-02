59ddc774d6e74fff0a52b717ec41b74908f4931f
Louis-Philippe Turmel <turmlo@2020spaces.com>
2021-09-01 22:36:53 -0400

    Renamed margin interface

--- src/props/margin.ts
+++ src/props/margin.ts
@@ -1,17 +1,17 @@
 import { TailwindRemValues } from "./Global";
 
-export default interface margin {
-    m?: TailwindRemValues;
-    my?: TailwindRemValues;
-    mx?: TailwindRemValues;
-    ml?: TailwindRemValues;
-    mt?: TailwindRemValues;
-    mr?: TailwindRemValues;
-    mb?: TailwindRemValues;
-    marginX?: TailwindRemValues;
-    marginY?: TailwindRemValues;
-    "margin-left"?: TailwindRemValues;
-    "margin-top"?: TailwindRemValues;
-    "margin-right"?: TailwindRemValues;
-    "margin-bottom"?: TailwindRemValues;
+export interface MarginProps {
+	m?: TailwindRemValues;
+	my?: TailwindRemValues;
+	mx?: TailwindRemValues;
+	ml?: TailwindRemValues;
+	mt?: TailwindRemValues;
+	mr?: TailwindRemValues;
+	mb?: TailwindRemValues;
+	marginX?: TailwindRemValues;
+	marginY?: TailwindRemValues;
+	"margin-left"?: TailwindRemValues;
+	"margin-top"?: TailwindRemValues;
+	"margin-right"?: TailwindRemValues;
+	"margin-bottom"?: TailwindRemValues;
 }
