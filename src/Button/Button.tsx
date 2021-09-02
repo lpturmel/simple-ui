7bf5a8026269d403db78b630c5ace75ad137bf5e
Louis-Philippe Turmel <turmlo@2020spaces.com>
2021-09-01 22:25:31 -0400

    Removed unused log

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -3,18 +3,18 @@ import MainProps from "../props";
 import { mapPropsToTw } from "../props/Global";
 
 interface ButtonProps
-    extends MainProps<JSX.ButtonHTMLAttributes<HTMLButtonElement>> {}
+	extends MainProps<JSX.ButtonHTMLAttributes<HTMLButtonElement>> {}
 
 const Button: Component<ButtonProps> = (props) => {
-    const tw = mapPropsToTw<PropsWithChildren<ButtonProps>>(props);
-    console.log(tw);
-    return (
-        <button
-            {...props}
-            class={`${tw} transition-colors duration-200 ease-in-out border border-transparent focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent font-semibold rounded-lg`}
-        >
-            {props.children}
-        </button>
-    );
+	const tw = mapPropsToTw<PropsWithChildren<ButtonProps>>(props);
+
+	return (
+		<button
+			{...props}
+			class={`${tw} transition-colors duration-200 ease-in-out border border-transparent focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent font-semibold rounded-lg`}
+		>
+			{props.children}
+		</button>
+	);
 };
 export default Button;
