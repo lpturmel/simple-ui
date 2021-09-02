bdbcebba09b71b7a02e61779fa58286cdc1fe5f3
Louis-Philippe Turmel <turmlo@2020spaces.com>
2021-09-01 23:10:32 -0400

    Updated Button Story

--- src/Button/Button.stories.tsx
+++ src/Button/Button.stories.tsx
@@ -1,22 +1,23 @@
 import Button from "./Button";
 
 export default {
-    component: Button,
-    title: "UI/Button",
+	component: Button,
+	title: "UI/Button",
 };
 
 export const Default = () => (
-    <Button
-        _hover={{
-            bg: "yellow.800",
-        }}
-        _active={{
-            bg: "yellow.900",
-        }}
-        bg="yellow.600"
-        px={4}
-        py={2}
-    >
-        Button
-    </Button>
+	<Button
+		_hover={{
+			bg: "yellow.500",
+		}}
+		_active={{
+			bg: "yellow.600",
+		}}
+		fontWeight="normal"
+		bg="yellow.400"
+		px={4}
+		py={2}
+	>
+		Button
+	</Button>
 );
