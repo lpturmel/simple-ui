9d57744038d97ae02818a6b7f26fd51855dd070d
Louis-Philippe Turmel <turmlo@2020spaces.com>
2021-09-01 22:55:45 -0400

    Merged main props

--- src/props/index.ts
+++ src/props/index.ts
@@ -3,11 +3,15 @@ import { MarginProps } from "./margin";
 import { PaddingProps } from "./padding";
 import { FontProps } from "./font";
 import { StateProps } from "./states";
+import { WidthProps } from "./width";
+import { HeightProps } from "./height";
 
 type MainProps<T = {}> = PaddingProps &
 	MarginProps &
 	ColorProps &
 	StateProps &
 	FontProps &
+	WidthProps &
+	HeightProps &
 	T;
 export default MainProps;
