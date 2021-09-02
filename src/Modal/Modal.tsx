bdd478124bb8b307ac3a1be83f3b35a940d2bc43
lpturmel <louisphilippe.turmel@gmail.com>
2021-09-02 01:02:55 -0400

    Added Modal Component

---
+++ src/Modal/Modal.tsx
@@ -0,0 +1,24 @@
+import { Component, JSX, useContext } from "solid-js";
+import { Portal } from "solid-js/web";
+import { SimpleContext } from "../context/SimpleContext";
+import MainProps from "../props";
+
+interface ModalProps extends MainProps<JSX.HTMLAttributes<HTMLDivElement>> {
+	isOpen: boolean;
+}
+const Modal: Component<ModalProps> = (props) => {
+	const [state, setState]: any = useContext(SimpleContext);
+
+	return (
+		<>
+			{props.isOpen && (
+				<Portal mount={state.portal}>
+					<div class="overflow-auto flex flex-col justify-center align-middle fixed h-screen w-screen top-0 left-0 z-50">
+						{props.children}
+					</div>
+				</Portal>
+			)}
+		</>
+	);
+};
+export default Modal;
