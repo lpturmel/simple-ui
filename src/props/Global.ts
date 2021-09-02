024f696b9ba75e9c0c3f1ac3b733400831d3fdad
Louis-Philippe Turmel <turmlo@2020spaces.com>
2021-09-01 22:37:59 -0400

    Added font to mapPropstoTw

--- src/props/Global.ts
+++ src/props/Global.ts
@@ -1,93 +1,107 @@
 import { parseStateProps } from "./states";
 
 export function mapPropsToTw<T>(props: T) {
-    // This type annotation is to have the proper types returned from Object.keys(x)
-    // By design it returns a string[]
-    let classString = "";
-    (Object.keys(props) as Array<keyof T>).forEach((prop) => {
-        let newPropName = prop.toString();
+	// This type annotation is to have the proper types returned from Object.keys(x)
+	// By design it returns a string[]
+	let classString = "";
+	(Object.keys(props) as Array<keyof T>).forEach((prop) => {
+		let newPropName = prop.toString();
 
-        switch (prop) {
-            /**
-             * Colors
-             */
-            case "color":
-                newPropName = "text";
-                break;
-            case "bg":
-                newPropName = "bg";
-                break;
-            case "backgroundColor":
-                newPropName = "bg";
-                break;
+		switch (prop) {
+			/**
+			 * Colors
+			 */
+			case "color":
+				newPropName = "text";
+				break;
+			case "bg":
+				newPropName = "bg";
+				break;
+			case "backgroundColor":
+				newPropName = "bg";
+				break;
+			/**
+			 * Font
+			 */
+			case "fontSize":
+				newPropName = "text";
+				break;
+			case "fontWeight":
+				newPropName = "font";
+				break;
+			/**
+			 * Utils
+			 */
+			case "placeholder":
+				return;
 
-            /**
-             * States
-             */
+			/**
+			 * States
+			 */
 
-            case "_active":
-                const activeProps = mapPropsToTw<T>(
-                    (props[prop] as unknown) as T
-                );
-                newPropName = parseStateProps(activeProps, "active");
+			case "_active":
+				const activeProps = mapPropsToTw<T>(
+					(props[prop] as unknown) as T
+				);
+				newPropName = parseStateProps(activeProps, "active");
 
-                break;
-            case "_focus":
-                const focusProps = mapPropsToTw<T>(
-                    (props[prop] as unknown) as T
-                );
+				break;
+			case "_focus":
+				const focusProps = mapPropsToTw<T>(
+					(props[prop] as unknown) as T
+				);
 
-                newPropName = parseStateProps(focusProps, "focus");
+				newPropName = parseStateProps(focusProps, "focus");
 
-                break;
+				break;
 
-            case "_hover":
-                const hoverProps = mapPropsToTw<T>(
-                    (props[prop] as unknown) as T
-                );
+			case "_hover":
+				const hoverProps = mapPropsToTw<T>(
+					(props[prop] as unknown) as T
+				);
 
-                newPropName = parseStateProps(hoverProps, "hover");
+				newPropName = parseStateProps(hoverProps, "hover");
 
-                break;
-        }
-        classString += `${newPropName}-${props[prop]} `.replace(".", "-");
-    });
-
-    return classString;
+				break;
+		}
+		classString += `${newPropName}-${props[prop]} `.replace(".", "-");
+	});
+	console.log(classString);
+	return classString;
 }
 
 export type TailwindRemValues =
-    | 0
-    | 0.5
-    | 1
-    | 1.5
-    | 2
-    | 2.5
-    | 3
-    | 3.5
-    | 4
-    | 5
-    | 6
-    | 7
-    | 8
-    | 9
-    | 10
-    | 11
-    | 12
-    | 14
-    | 16
-    | 20
-    | 24
-    | 28
-    | 32
-    | 36
-    | 40
-    | 44
-    | 48
-    | 52
-    | 56
-    | 60
-    | 64
-    | 72
-    | 80
-    | 96;
+	| 0
+	| 0.5
+	| 1
+	| 1.5
+	| 2
+	| 2.5
+	| 3
+	| 3.5
+	| 4
+	| 5
+	| 6
+	| 7
+	| 8
+	| 9
+	| 10
+	| 11
+	| 12
+	| 14
+	| 16
+	| 20
+	| 24
+	| 28
+	| 32
+	| 36
+	| 40
+	| 44
+	| 48
+	| 52
+	| 56
+	| 60
+	| 64
+	| 72
+	| 80
+	| 96;
