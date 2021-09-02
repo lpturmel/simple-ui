6d0f87136fee07b859ca687b090b6d82117f1b2c
Louis-Philippe Turmel <turmlo@2020spaces.com>
2021-09-01 23:10:12 -0400

    Updated Input base component

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -3,14 +3,14 @@ import MainProps from "../props";
 import { mapPropsToTw } from "../props/Global";
 
 interface InputProps
-    extends MainProps<JSX.InputHTMLAttributes<HTMLInputElement>> {}
+	extends MainProps<JSX.InputHTMLAttributes<HTMLInputElement>> {}
 const Input: Component<InputProps> = (props) => {
-    const tw = mapPropsToTw<PropsWithChildren<InputProps>>(props);
-    return (
-        <input
-            {...props}
-            class={`${tw} transition-colors duration-200 ease-in-out hover:bg-gray-300 py-2 px-4 bg-gray-100 border border-transparent focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent font-semibold rounded-lg placeholder-gray-600"`}
-        />
-    );
+	const tw = mapPropsToTw<PropsWithChildren<InputProps>>(props);
+	return (
+		<input
+			{...props}
+			class={`${tw}  transition-colors duration-200 ease-in-out border border-transparent focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent  rounded-lg placeholder-gray-700`}
+		/>
+	);
 };
 export default Input;
