eab7e00c43e09914fede1a15b638f028ed221e49
Louis-Philippe Turmel <turmlo@2020spaces.com>
2021-09-01 23:10:20 -0400

    Updated Input Story

--- src/Input/Input.stories.tsx
+++ src/Input/Input.stories.tsx
@@ -2,8 +2,17 @@ import Button from "../Button/Button";
 import Input from "./Input";
 
 export default {
-    component: Input,
-    title: "UI/Input",
+	component: Input,
+	title: "UI/Input",
 };
 
-export const Default = () => <Input placeholder="Regular Input" />;
+export const Default = () => (
+	<Input
+		placeholder="Regular Input"
+		py={2}
+		px={4}
+		bg={"gray.300"}
+		_hover={{ bg: "gray.400" }}
+		_focus={{ bg: "white" }}
+	/>
+);
