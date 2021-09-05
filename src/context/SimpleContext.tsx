896cecbce7954ff6feb51b786d4d9af0f23970ec
Louis-Philippe <louisphilippe.turmel@gmail.com>
2021-09-04 20:08:06 -0400

    Removed wrong import

--- src/context/SimpleContext.tsx
+++ src/context/SimpleContext.tsx
@@ -4,26 +4,27 @@ import "../props/sizing/width/index.css";
 import "../props/sizing/height/index.css";
 import "../props/background/colors/index.css";
 import "../props/text/colors/index.css";
-import "../props/colors/index.css";
+import "../props/text/fontSize/index.css";
+import "../props/text/fontWeight/index.css";
 
 export const SimpleContext = createContext();
 
 export const SimpleProvider: Component = (props) => {
-	const [state, setState] = createStore<{
-		portal: null | HTMLDivElement;
-	}>({
-		portal: null,
-	});
+    const [state, setState] = createStore<{
+        portal: null | HTMLDivElement;
+    }>({
+        portal: null,
+    });
 
-	return (
-		<SimpleContext.Provider value={[state, setState]}>
-			<div>
-				<div
-					ref={(el) => setState("portal", el)}
-					id="simple-ui-portal"
-				/>
-				{props.children}
-			</div>
-		</SimpleContext.Provider>
-	);
+    return (
+        <SimpleContext.Provider value={[state, setState]}>
+            <div>
+                <div
+                    ref={(el) => setState("portal", el)}
+                    id="simple-ui-portal"
+                />
+                {props.children}
+            </div>
+        </SimpleContext.Provider>
+    );
 };
