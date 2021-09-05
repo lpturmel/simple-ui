9f4b70e38e4cefa06ac8ca1fb174cefb844545d5
Louis-Philippe <louisphilippe.turmel@gmail.com>
2021-09-04 20:15:00 -0400

    Added backgroundColor props

---
+++ src/props/background/colors/index.ts
@@ -0,0 +1,7 @@
+import { SimpleColorValues } from "../../Global";
+
+export interface ColorProps {
+    backgroundColor?: SimpleColorValues;
+    bg?: SimpleColorValues;
+    color?: SimpleColorValues;
+}
