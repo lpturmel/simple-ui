5594fa5b2c4ef1b231265f37ec0a223b92e2a873
lpturmel <louisphilippe.turmel@gmail.com>
2021-09-04 20:21:34 -0400

    Imported borderRadius file inside main context

--- src/context/SimpleContext.tsx
+++ src/context/SimpleContext.tsx
@@ -6,25 +6,26 @@ import "../props/background/colors/index.css";
 import "../props/text/colors/index.css";
 import "../props/text/fontSize/index.css";
 import "../props/text/fontWeight/index.css";
+import "../props/borders/borderRadius/index.css";
 
 export const SimpleContext = createContext();
 
 export const SimpleProvider: Component = (props) => {
-    const [state, setState] = createStore<{
-        portal: null | HTMLDivElement;
-    }>({
-        portal: null,
-    });
+	const [state, setState] = createStore<{
+		portal: null | HTMLDivElement;
+	}>({
+		portal: null,
+	});
 
-    return (
-        <SimpleContext.Provider value={[state, setState]}>
-            <div>
-                <div
-                    ref={(el) => setState("portal", el)}
-                    id="simple-ui-portal"
-                />
-                {props.children}
-            </div>
-        </SimpleContext.Provider>
-    );
+	return (
+		<SimpleContext.Provider value={[state, setState]}>
+			<div>
+				<div
+					ref={(el) => setState("portal", el)}
+					id="simple-ui-portal"
+				/>
+				{props.children}
+			</div>
+		</SimpleContext.Provider>
+	);
 };
