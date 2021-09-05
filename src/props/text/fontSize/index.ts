614cb944128e5db31ba2714adcff16104c7ab0eb
Louis-Philippe <louisphilippe.turmel@gmail.com>
2021-09-04 20:07:14 -0400

    Added fontSize props and type values

---
+++ src/props/text/fontSize/index.ts
@@ -0,0 +1,18 @@
+export interface FontSizeProps {
+    fontSize?: FontSizeValues;
+}
+
+export type FontSizeValues =
+    | "xs"
+    | "sm"
+    | "base"
+    | "lg"
+    | "xl"
+    | "2xl"
+    | "3xl"
+    | "4xl"
+    | "5xl"
+    | "6xl"
+    | "7xl"
+    | "8xl"
+    | "9xl";
