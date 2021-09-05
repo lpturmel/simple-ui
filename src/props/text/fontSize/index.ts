14f4ac5fe5d78210143b76541a4b312d868df46e
lpturmel <louisphilippe.turmel@gmail.com>
2021-09-04 21:16:53 -0400

    Changed FontSize type

--- src/props/text/fontSize/index.ts
+++ src/props/text/fontSize/index.ts
@@ -1,18 +1,18 @@
 export interface FontSizeProps {
-    fontSize?: FontSizeValues;
+	fontSize?: FontSizeValues;
 }
 
 export type FontSizeValues =
-    | "xs"
-    | "sm"
-    | "base"
-    | "lg"
-    | "xl"
-    | "2xl"
-    | "3xl"
-    | "4xl"
-    | "5xl"
-    | "6xl"
-    | "7xl"
-    | "8xl"
-    | "9xl";
+	| "xs"
+	| "sm"
+	| "md"
+	| "lg"
+	| "xl"
+	| "2xl"
+	| "3xl"
+	| "4xl"
+	| "5xl"
+	| "6xl"
+	| "7xl"
+	| "8xl"
+	| "9xl";
