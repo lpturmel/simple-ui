a61dc92acfda76e46701dcee039641774a9097ec
lpturmel <louisphilippe.turmel@gmail.com>
2021-09-08 18:23:28 -0400

    Added PlaceItems to Main Context

--- src/context/SimpleContext.tsx
+++ src/context/SimpleContext.tsx
@@ -27,6 +27,7 @@ import "../props/flex/alignContent/index.css";
 import "../props/flex/alignItems/index.css";
 import "../props/flex/alignSelf/index.css";
 import "../props/flex/placeContent/index.css";
+import "../props/flex/placeItems/index.css";
 import "../props/grid/gridTemplateColumns/index.css";
 import "../props/grid/gridTemplateRows/index.css";
 import "../props/grid/gridColSpan/index.css";
