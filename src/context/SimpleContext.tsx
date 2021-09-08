54272a165ee26492c949a8c43807009e97be15f9
lpturmel <louisphilippe.turmel@gmail.com>
2021-09-08 18:26:28 -0400

    Added PlaceSelf to Main  Context

--- src/context/SimpleContext.tsx
+++ src/context/SimpleContext.tsx
@@ -28,6 +28,7 @@ import "../props/flex/alignItems/index.css";
 import "../props/flex/alignSelf/index.css";
 import "../props/flex/placeContent/index.css";
 import "../props/flex/placeItems/index.css";
+import "../props/flex/placeSelf/index.css";
 import "../props/grid/gridTemplateColumns/index.css";
 import "../props/grid/gridTemplateRows/index.css";
 import "../props/grid/gridColSpan/index.css";
