5974e579172cdc1866a9a3ee64f926b4e22165da
lpturmel <louisphilippe.turmel@gmail.com>
2021-09-08 16:13:42 -0400

    Added gridTemplateColumns type declarations & values

---
+++ src/props/grid/gridTemplateColumns/index.ts
@@ -0,0 +1,18 @@
+export interface GridTemplateColumnProps {
+	gridCols: GridTemplateColumnValues;
+}
+
+export type GridTemplateColumnValues =
+	| 1
+	| 2
+	| 3
+	| 4
+	| 5
+	| 6
+	| 7
+	| 8
+	| 9
+	| 10
+	| 11
+	| 12
+	| "none";
