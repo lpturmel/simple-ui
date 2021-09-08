be8c0c65774c2849e2e55395d2f0c527b49b8c4d
lpturmel <louisphilippe.turmel@gmail.com>
2021-09-08 18:17:48 -0400

    Added PlaceContent type declarations & values

---
+++ src/props/flex/placeContent/index.ts
@@ -0,0 +1,12 @@
+export interface PlaceContentProps {
+	placeContent: PlaceContentValues;
+}
+
+export type PlaceContentValues =
+	| "center"
+	| "start"
+	| "end"
+	| "between"
+	| "around"
+	| "evenly"
+	| "stretch";
