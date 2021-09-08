afdee6b8acafef160512beef6467199dc49407c1
lpturmel <louisphilippe.turmel@gmail.com>
2021-09-08 17:45:35 -0400

    Added order type  declarations & values

---
+++ src/props/flex/order/index.ts
@@ -0,0 +1,20 @@
+export interface OrderProps {
+	order: OrderValues;
+}
+
+export type OrderValues =
+	| "1"
+	| "2"
+	| "3"
+	| "4"
+	| "5"
+	| "6"
+	| "7"
+	| "8"
+	| "9"
+	| "10"
+	| "11"
+	| "12"
+	| "first"
+	| "last"
+	| "none";
