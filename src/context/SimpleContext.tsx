9e51ca62cee58f5396368bff63f46bd90bc575af
lpturmel <louisphilippe.turmel@gmail.com>
2021-09-08 18:12:51 -0400

    Added AlignSelf to MainContext

--- src/context/SimpleContext.tsx
+++ src/context/SimpleContext.tsx
@@ -25,6 +25,7 @@ import "../props/flex/justifyItems/index.css";
 import "../props/flex/justifySelf/index.css";
 import "../props/flex/alignContent/index.css";
 import "../props/flex/alignItems/index.css";
+import "../props/flex/alignSelf/index.css";
 import "../props/grid/gridTemplateColumns/index.css";
 import "../props/grid/gridTemplateRows/index.css";
 import "../props/grid/gridColSpan/index.css";
