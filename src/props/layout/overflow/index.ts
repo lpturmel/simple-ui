b9b9fc5c528a994a0ba9e5803680793b0b2ea930
Louis-Philippe <louisphilippe.turmel@gmail.com>
2021-09-20 16:11:26 -0400

    Changed props to optional

--- src/props/layout/overflow/index.ts
+++ src/props/layout/overflow/index.ts
@@ -1,7 +1,7 @@
 export interface OverflowProps {
-	overflow: overflowValues;
-	overflowX: overflowValues;
-	overflowY: overflowValues;
+	overflow?: overflowValues;
+	overflowX?: overflowValues;
+	overflowY?: overflowValues;
 }
 
 export type overflowValues = "auto" | "hidden" | "visible" | "scroll";
