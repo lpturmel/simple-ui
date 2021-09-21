236d8ed0b6c101131e2fef0ca67e5064f2fa725c
Louis-Philippe <louisphilippe.turmel@gmail.com>
2021-09-20 22:03:47 -0400

    Added ModalOverlay default props

--- src/components/Modal/ModalOverlay/index.tsx
+++ src/components/Modal/ModalOverlay/index.tsx
@@ -1,11 +1,25 @@
 import { Component, JSX, PropsWithChildren } from "solid-js";
+import MainProps from "../../../props";
+import { parseDefaultProps, mapPropsToCss } from "../../../props/Global";
 
-const ModalOverlay: Component = (props) => {
-	return (
-		<div
-			class="fixed h-screen w-screen top-0 left-0"
-			style={{ "z-index": 10, color: "rgba(0, 0, 0, 0.48)" }}
-		></div>
+export interface ModalOverlayProps
+	extends MainProps<JSX.HTMLAttributes<HTMLDivElement>> {}
+const ModalOverlay: Component<ModalOverlayProps> = (props) => {
+	const defaultProps: PropsWithChildren<ModalOverlayProps> = {
+		top: 0,
+		left: 0,
+		position: "fixed",
+		height: "screen",
+		width: "screen",
+
+		zIndex: 10,
+	};
+	const mergedProps = parseDefaultProps<ModalOverlayProps>(
+		props,
+		defaultProps
 	);
+
+	const tw = mapPropsToCss<PropsWithChildren<ModalOverlayProps>>(mergedProps);
+	return <div class={tw} style={{ color: "rgba(0, 0, 0, 0.48)" }}></div>;
 };
 export default ModalOverlay;
