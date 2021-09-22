abcb69e6158c1fb467fc1408e69087541d79a620
lpturmel <louisphilippe.turmel@gmail.com>
2021-09-22 15:53:23 -0400

    Added maxWidth type declarations

---
+++ src/props/sizing/maxWidth/index.ts
@@ -0,0 +1,28 @@
+export interface MaxWidthProps {
+	maxW?: MaxWidthValues;
+	maxWidth?: MaxWidthValues;
+}
+
+export type MaxWidthValues =
+	| 0
+	| "none"
+	| "xs"
+	| "sm"
+	| "md"
+	| "lg"
+	| "xl"
+	| "2xl"
+	| "3xl"
+	| "4xl"
+	| "5xl"
+	| "6xl"
+	| "7xl"
+	| "full"
+	| "min"
+	| "max"
+	| "prose"
+	| "screen-sm"
+	| "screen-md"
+	| "screen-lg"
+	| "screen-xl"
+	| "screen-2xl";
