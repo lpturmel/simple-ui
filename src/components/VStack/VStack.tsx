8c969d203be581f153e14873287e45cb7205c560
lpturmel <louisphilippe.turmel@gmail.com>
2021-09-22 15:54:12 -0400

    Changed VStack map function to new mapPropsToCss

--- src/components/VStack/VStack.tsx
+++ src/components/VStack/VStack.tsx
@@ -1,11 +1,11 @@
 import { Component, JSX, PropsWithChildren } from "solid-js";
 import MainProps from "../../props";
-import { mapPropsToTw } from "../../props/Global";
+import { mapPropsToCss } from "../../props/Global";
 
 export interface VStackProps
 	extends MainProps<JSX.HTMLAttributes<HTMLDivElement>> {}
 export const VStack: Component<VStackProps> = (props) => {
-	const tw = mapPropsToTw<PropsWithChildren<VStackProps>>(props);
+	const tw = mapPropsToCss<PropsWithChildren<VStackProps>>(props);
 
 	return (
 		<div {...props} class={tw}>
