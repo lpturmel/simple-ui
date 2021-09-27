8d08e266a054ff43c6d5e20d184bbfe7be101a46
Louis-Philippe <louisphilippe.turmel@gmail.com>
2021-09-27 11:41:27 -0400

    Exported all modal components in main index

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,16 @@ export * from "./components/Button";
 export * from "./components/Select";
 export * from "./components/Input";
 export * from "./apis/createControls";
+export * from "./components/Modal";
+export * from "./components/Modal/ModalBody";
+export * from "./components/Modal/ModalHeader";
+export * from "./components/Modal/ModalOverlay";
+export * from "./components/Modal/ModalContent";
+export * from "./components/Modal/ModalFooter";
+
 export { SimpleProvider } from "./context/SimpleContext";
 export {
 	SimpleComponentList,
 	ThemeComponentOptions,
 	SimpleThemeConfig,
 } from "./theme/index";
-
-export * from "./components/Modal";
