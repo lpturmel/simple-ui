2be2258b0747d6a0be9c9927b8aad3fb9cec8530
Louis-Philippe <louisphilippe.turmel@gmail.com>
2021-09-27 11:49:47 -0400

    Added ModalFooter theme options in a separate file

---
+++ src/components/Modal/ModalFooter/theme.ts
@@ -0,0 +1,10 @@
+import { ThemeComponentOptions } from "../../..";
+
+const ModalFooterOptions: ThemeComponentOptions = {
+	defaultProps: {
+		paddingX: 4,
+		paddingY: 4,
+	},
+};
+
+export default ModalFooterOptions;
