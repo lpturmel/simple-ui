d13c8f7af2a5ccc8d3b2ad9afa864aa5575d2527
Louis-Philippe <louisphilippe.turmel@gmail.com>
2021-09-27 11:49:18 -0400

    Changed components declaration to use external files

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,5 +1,13 @@
 import ButtonOptions from "../components/Button/theme";
 import HStackOptions from "../components/HStack/theme";
+import InputOptions from "../components/Input/theme";
+import ModalBodyOptions from "../components/Modal/ModalBody/theme";
+import ModalContentOptions from "../components/Modal/ModalContent/theme";
+import ModalFooterOptions from "../components/Modal/ModalFooter/theme";
+import ModalHeaderOptions from "../components/Modal/ModalHeader/theme";
+import ModalOverlayOptions from "../components/Modal/ModalOverlay/theme";
+import ModalOptions from "../components/Modal/theme";
+import VStackOptions from "../components/VStack/theme";
 import MainProps from "../props";
 
 export interface SimpleComponentList {
@@ -18,7 +26,10 @@ export interface SimpleComponentList {
 
 export interface ThemeComponentOptions {
 	defaultProps?: MainProps;
-	variants?: any; // TODO
+	/**
+	 * TODO
+	 */
+	variants?: unknown;
 }
 export interface SimpleThemeConfig {
 	Components?: SimpleComponentList;
@@ -33,81 +44,28 @@ export const DefaultTheme: SimpleThemeConfig = {
 			...HStackOptions,
 		},
 		VStack: {
-			defaultProps: {
-				display: "flex",
-				flexDirection: "col",
-			},
+			...VStackOptions,
 		},
 		Input: {
-			defaultProps: {},
+			...InputOptions,
 		},
 		Modal: {
-			defaultProps: {
-				display: "flex",
-				flexDirection: "col",
-				overflow: "auto",
-				justifyContent: "start",
-				alignItems: "center",
-				position: "fixed",
-				height: "screen",
-				width: "screen",
-				top: 0,
-				left: 0,
-				zIndex: 50,
-			},
+			...ModalOptions,
 		},
 		ModalOverlay: {
-			defaultProps: {
-				top: 0,
-				left: 0,
-				position: "fixed",
-				height: "screen",
-				width: "screen",
-
-				zIndex: 10,
-			},
+			...ModalOverlayOptions,
 		},
 		ModalHeader: {
-			defaultProps: {
-				display: "flex",
-				flexDirection: "row",
-				justifyContent: "between",
-				paddingX: 4,
-				paddingY: 4,
-				fontSize: "xl",
-				fontWeight: "semibold",
-			},
+			...ModalHeaderOptions,
 		},
 		ModalFooter: {
-			defaultProps: {
-				paddingX: 4,
-				paddingY: 4,
-			},
+			...ModalFooterOptions,
 		},
 		ModalContent: {
-			defaultProps: {
-				// add maxHeight: max-h-32
-				position: "relative",
-				maxW: "md",
-				margin: "auto",
-				w: "full",
-				display: "flex",
-				flexDirection: "col",
-				p: 2,
-				maxWidth: "md",
-				marginT: 16,
-				marginB: 16,
-				bg: "gray.100",
-				borderRadius: "md",
-				zIndex: 50,
-			},
+			...ModalContentOptions,
 		},
 		ModalBody: {
-			defaultProps: {
-				zIndex: 50,
-				paddingX: 4,
-				paddingY: 4,
-			},
+			...ModalBodyOptions,
 		},
 	},
 };
