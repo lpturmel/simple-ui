f9004c0659793bdb3c5b6940e106faac5f02573a
lpturmel <louisphilippe.turmel@gmail.com>
2021-09-26 20:53:54 -0400

    Changed Button to use global theme

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,5 @@
-import { Component, JSX, PropsWithChildren } from "solid-js";
+import { Component, JSX, PropsWithChildren, useContext } from "solid-js";
+import { SimpleContext } from "../../context/SimpleContext";
 import MainProps from "../../props";
 import { mapPropsToCss, parseDefaultProps } from "../../props/Global";
 
@@ -6,17 +7,15 @@ export interface ButtonProps
 	extends MainProps<JSX.ButtonHTMLAttributes<HTMLButtonElement>> {}
 
 export const Button: Component<ButtonProps> = (props) => {
-	const defaultProps: PropsWithChildren<ButtonProps> = {
-		px: 4,
-		py: 2,
-		borderRadius: "md",
-		fontSize: "lg",
-		fontWeight: "semibold",
-		color: "white",
-	};
+	const [context] = useContext(SimpleContext);
 
-	const mergedProps = parseDefaultProps<ButtonProps>(props, defaultProps);
+	const buttonDefaultProps = context.theme.Components?.Button;
 
+	const mergedProps = parseDefaultProps<ButtonProps>(
+		props,
+		buttonDefaultProps!
+	);
+	// Add
 	const tw = mapPropsToCss<PropsWithChildren<ButtonProps>>(mergedProps);
 
 	return (
