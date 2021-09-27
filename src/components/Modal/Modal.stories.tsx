3bc8b77f0570cafe3cb6cc708045f065306ed553
Louis-Philippe <louisphilippe.turmel@gmail.com>
2021-09-27 11:50:36 -0400

    Changed story to use new Modal prop isOpen and controls which gives the accessor directly

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -6,28 +6,35 @@ import { SimpleProvider } from "../../context/SimpleContext";
 import ModalContent from "./ModalContent";
 import createControls from "../../apis/createControls";
 import { Button } from "../Button/Button";
+import { HStack } from "../HStack";
 
 export default {
 	component: Modal,
 	title: "UI/Modal",
 };
 
-const { isOpen, open, close } = createControls();
-
+const controls = createControls();
 export const Default = () => (
 	<SimpleProvider>
-		<Button onClick={open} bg="blue.200" px={4} py={2}>
+		<Button onClick={controls.open} bg="blue.200" px={4} py={2}>
 			Open Modal
 		</Button>
-		<Modal isOpen={isOpen()}>
+		<Modal isOpen={controls.isOpen} controls={controls}>
 			<ModalOverlay />
 			<ModalContent>
 				<ModalHeader>Test</ModalHeader>
 				<ModalBody>
-					<Button onClick={close} bg="red.200" px={4} py={2}>
-						Close
-					</Button>
-					<p>beau body</p>
+					<HStack spacingX={4}>
+						<Button
+							onClick={controls.close}
+							bg="red.200"
+							px={4}
+							py={2}
+						>
+							Close
+						</Button>
+						<p>beau body</p>
+					</HStack>
 				</ModalBody>
 			</ModalContent>
 		</Modal>
