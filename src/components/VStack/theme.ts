39f37ab03484f73d1434e99bc20c4e1dfc4e59bb
Louis-Philippe <louisphilippe.turmel@gmail.com>
2021-09-27 11:49:26 -0400

    Added VStack theme options in a separate file

---
+++ src/components/VStack/theme.ts
@@ -0,0 +1,10 @@
+import { ThemeComponentOptions } from "../..";
+
+const VStackOptions: ThemeComponentOptions = {
+	defaultProps: {
+		display: "flex",
+		flexDirection: "col",
+	},
+};
+
+export default VStackOptions;
