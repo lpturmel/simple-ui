92ca86f99961b28d8dee9391578d6f818e614f1f
Louis-Philippe <louisphilippe.turmel@gmail.com>
2021-09-27 11:50:01 -0400

    Added ModalBody theme options in a separate file

---
+++ src/components/Modal/ModalBody/theme.ts
@@ -0,0 +1,11 @@
+import { ThemeComponentOptions } from "../../..";
+
+const ModalBodyOptions: ThemeComponentOptions = {
+	defaultProps: {
+		zIndex: 50,
+		paddingX: 4,
+		paddingY: 4,
+	},
+};
+
+export default ModalBodyOptions;
