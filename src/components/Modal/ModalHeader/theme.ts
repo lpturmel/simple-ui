6dd8cf5dea8906e44862caaa49a0d97f0fca24f4
Louis-Philippe <louisphilippe.turmel@gmail.com>
2021-09-27 11:49:41 -0400

    Added ModalHeader theme options in a separate file

---
+++ src/components/Modal/ModalHeader/theme.ts
@@ -0,0 +1,15 @@
+import { ThemeComponentOptions } from "../../..";
+
+const ModalHeaderOptions: ThemeComponentOptions = {
+	defaultProps: {
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "between",
+		paddingX: 4,
+		paddingY: 4,
+		fontSize: "xl",
+		fontWeight: "semibold",
+	},
+};
+
+export default ModalHeaderOptions;
