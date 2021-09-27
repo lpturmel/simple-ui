b1622572698c31335b8a918308cdc304263e483c
Louis-Philippe <louisphilippe.turmel@gmail.com>
2021-09-27 11:39:55 -0400

    Added HStack theme options in a separate file

---
+++ src/components/HStack/theme.ts
@@ -0,0 +1,10 @@
+import { ThemeComponentOptions } from "../../theme";
+
+const HStackOptions: ThemeComponentOptions = {
+	defaultProps: {
+		display: "flex",
+		flexDirection: "row",
+	},
+};
+
+export default HStackOptions;
