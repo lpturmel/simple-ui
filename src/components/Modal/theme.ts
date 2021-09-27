7714840c1b61465c19b5af81f5457bbe74029fac
Louis-Philippe <louisphilippe.turmel@gmail.com>
2021-09-27 11:50:14 -0400

    Added Modal theme options in a separate file

---
+++ src/components/Modal/theme.ts
@@ -0,0 +1,19 @@
+import { ThemeComponentOptions } from "../..";
+
+const ModalOptions: ThemeComponentOptions = {
+	defaultProps: {
+		display: "flex",
+		flexDirection: "col",
+		overflow: "auto",
+		justifyContent: "start",
+		alignItems: "center",
+		position: "fixed",
+		height: "screen",
+		width: "screen",
+		top: 0,
+		left: 0,
+		zIndex: 50,
+	},
+};
+
+export default ModalOptions;
