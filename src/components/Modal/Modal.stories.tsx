d5f5da64dc4f5c94f77b9c6d97f0e359f3f13cb3
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-02 00:37:29 -0400

    Match Modal sub components import changes

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -1,12 +1,12 @@
-import ModalOverlay from "./ModalOverlay";
-import ModalBody from "./ModalBody";
+import { ModalOverlay } from "./ModalOverlay";
+import { ModalBody } from "./ModalBody";
 import { Modal } from ".";
-import ModalHeader from "./ModalHeader";
+import { ModalHeader } from "./ModalHeader";
 import { SimpleProvider } from "../../context/SimpleContext";
-import ModalContent from "./ModalContent";
+import { ModalContent } from "./ModalContent";
 import createControls from "../../apis/createControls";
 import { Button } from "../Button/Button";
-import ModalFooter from "./ModalFooter";
+import { ModalFooter } from "./ModalFooter";
 import { HStack } from "../HStack";
 
 export default {
