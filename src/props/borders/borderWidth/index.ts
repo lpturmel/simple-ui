b66f32fd06149fb2096b5ef08b360fc86bf57e06
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-03 12:59:47 -0400

    Added borderWidth type definitions

---
+++ src/props/borders/borderWidth/index.ts
@@ -0,0 +1,9 @@
+export interface BorderWidthProps {
+	borderWidth?: BorderWidthValues;
+	borderWidthRight?: BorderWidthValues;
+	borderWidthTop?: BorderWidthValues;
+	borderWidthBottom?: BorderWidthValues;
+	borderWidthLeft?: BorderWidthValues;
+}
+
+export type BorderWidthValues = 0 | 1 | 2 | 4 | 8;
