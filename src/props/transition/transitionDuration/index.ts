68db3eaa2a31ec2e822eeeb1ad402f6264bdaeaa
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-03 13:41:15 -0400

    Added transitionDuration type declarations

---
+++ src/props/transition/transitionDuration/index.ts
@@ -0,0 +1,13 @@
+export interface TransitionDurationProps {
+	transitionDuration?: TransitionDurationValues;
+}
+
+export type TransitionDurationValues =
+	| 75
+	| 100
+	| 150
+	| 200
+	| 300
+	| 500
+	| 700
+	| 1000;
