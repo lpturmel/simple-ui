5fc069d033166c93872be0641b57754f25e555fe
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-03 13:13:59 -0400

    Added borderColor to main context

--- src/context/SimpleContext.tsx
+++ src/context/SimpleContext.tsx
@@ -13,6 +13,7 @@ import "../props/text/fontSize/index.css";
 import "../props/text/fontWeight/index.css";
 import "../props/borders/borderRadius/index.css";
 import "../props/borders/borderWidth/index.css";
+import "../props/borders/borderColor/index.css";
 import "../props/spacing/margin/index.css";
 import "../props/spacing/padding/index.css";
 import "../props/spacing/spaceBetween/index.css";
