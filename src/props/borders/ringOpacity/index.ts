c12288487e917dc8cb56598fedc6c1c1bed9de96
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-03 17:49:49 -0400

    Updated ringOpacity type declarations

---
+++ src/props/borders/ringOpacity/index.ts
@@ -0,0 +1,20 @@
+export interface RingOpacityProps {
+	ringOpacity?: RingOpacityValues;
+}
+
+export type RingOpacityValues =
+	| 0
+	| 5
+	| 10
+	| 20
+	| 25
+	| 30
+	| 40
+	| 50
+	| 60
+	| 70
+	| 75
+	| 80
+	| 90
+	| 95
+	| 100;
