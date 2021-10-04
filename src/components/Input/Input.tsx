2a6b0981f1f5f9e02421f824db2041e6986f9625
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-03 23:29:14 -0400

    Removed unused tailwindCSS classes inside Input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -9,10 +9,6 @@ export const Input: Component<InputProps> = (props) => {
 	const mergedProps = mergeProps("Input", props);
 
 	const simpleProps = mapPropsToCss(mergedProps, true);
-	return (
-		<input
-			{...props}
-			class={`${simpleProps} transition-colors duration-200 ease-in-out border border-transparent focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent  rounded-lg placeholder-gray-700`}
-		/>
-	);
+
+	return <input {...props} class={simpleProps} />;
 };
