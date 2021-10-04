49cfd860cbe464dda0d380302a0a531d092ef614
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-03 23:29:34 -0400

    Added default theme theming to input

--- src/components/Input/theme.ts
+++ src/components/Input/theme.ts
@@ -1,7 +1,20 @@
 import { ThemeComponentOptions } from "../../theme";
 
+// ("transition-colors duration-200 ease-in-out border border-transparent focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent  rounded-lg placeholder-gray-700");
 const InputOptions: ThemeComponentOptions = {
-	defaultProps: {},
+	defaultProps: {
+		transition: "colors",
+		transitionDuration: 200,
+		transitionTiming: "ease-in-out",
+		borderWidth: 1,
+		borderColor: "transparent",
+		_focus: {
+			ringWidth: 2,
+
+			borderColor: "transparent",
+		},
+		borderRadius: "lg",
+	},
 };
 
 export default InputOptions;
