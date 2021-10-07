1b604743577ccb17fac93ece7ce081b7f777747e
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-07 14:38:10 -0400

    Added simple-ui input instead of classic DOM one

--- website/src/components/Navbar.tsx
+++ website/src/components/Navbar.tsx
@@ -1,12 +1,12 @@
-import { HStack } from "simple-ui-solid";
+import { HStack, Input } from "simple-ui-solid";
 import { Component } from "solid-js";
 
 const Navbar: Component = (props) => {
 	return (
 		<HStack w="full" justifyContent="between" py={2} px={4}>
 			<a href="/"> Simple-ui </a>
-			<input width={300} placeholder="search docs" />
 
+			<Input bg="gray.100" w={64} p={2} placeholder="Search docs" />
 			<p> v0.0.17 </p>
 		</HStack>
 	);
