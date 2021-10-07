7bbb16b0557e911833c0f54f42c6eb443af4fde3
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-07 14:37:56 -0400

    Removed layout from component content

--- website/src/components/Button.tsx
+++ website/src/components/Button.tsx
@@ -8,14 +8,12 @@ Prism.highlightAll();
 const ButtonComponent: Component = (props) => {
 	const code = `<Button>Button</Button>`;
 	return (
-		<Layout>
-			<VStack spacingY={4} alignItems="start">
-				<Button>Button</Button>
-				<pre>
-					<code class="prism-code language-js">{code}</code>
-				</pre>
-			</VStack>
-		</Layout>
+		<VStack spacingY={4} alignItems="start">
+			<Button>Button</Button>
+			<pre>
+				<code class="prism-code language-js">{code}</code>
+			</pre>
+		</VStack>
 	);
 };
 export default ButtonComponent;
--- website/src/components/Modal.tsx
+++ website/src/components/Modal.tsx
@@ -10,7 +10,6 @@ import {
 	HStack,
 	Button,
 	createControls,
-	SimpleProvider,
 } from "simple-ui-solid";
 import Prism from "prismjs";
 
@@ -18,7 +17,6 @@ Prism.highlightAll();
 
 const ModalComponent: Component = (props) => {
 	const controls = createControls();
-
 	const code = `
 const Modal: Component = (props) => {
     const controls = createControls();
@@ -54,46 +52,43 @@ const Modal: Component = (props) => {
     `;
 
 	return (
-		<Layout>
-			<SimpleProvider>
-				<Button onClick={controls.open} bg="blue.200" px={4} py={2}>
-					Open Modal
-				</Button>
-				<Modal isOpen={controls.isOpen} controls={controls}>
-					<ModalOverlay />
-					<ModalContent>
-						<ModalHeader>I am a header</ModalHeader>
-						<ModalBody>
-							Sit nulla est ex deserunt exercitation anim
-							occaecat. Nostrud ullamco deserunt aute id consequat
-							veniam incididunt duis in sint irure nisi. Mollit
-							officia cillum Lorem ullamco minim nostrud elit
-							officia tempor esse quis. Sunt ad dolore quis aute
-							consequat. Magna exercitation reprehenderit magna
-							aute tempor cupidatat consequat elit dolor
-							adipisicing. Mollit dolor eiusmod sunt ex incididunt
-							cillum quis. Velit duis sit officia eiusmod Lorem
-							aliqua enim laboris do dolor eiusmod. Et mollit
-							incididunt nisi consectetur esse laborum eiusmod
-							pariatur proident Lorem eiusmod et. Culpa deserunt
-							nostrud ad veniam.
-						</ModalBody>
-						<ModalFooter>
-							<HStack w="full" justifyContent="end" spacingX={2}>
-								<Button bg="gray.400" onClick={controls.close}>
-									Cancel
-								</Button>
-								<Button bg="blue.400">Save</Button>
-							</HStack>
-						</ModalFooter>
-					</ModalContent>
-				</Modal>
-			</SimpleProvider>
+		<>
+			<Button onClick={controls.open} bg="blue.200" px={4} py={2}>
+				Open Modal
+			</Button>
+			<Modal isOpen={controls.isOpen} controls={controls}>
+				<ModalOverlay />
+				<ModalContent>
+					<ModalHeader>I am a header</ModalHeader>
+					<ModalBody>
+						Sit nulla est ex deserunt exercitation anim occaecat.
+						Nostrud ullamco deserunt aute id consequat veniam
+						incididunt duis in sint irure nisi. Mollit officia
+						cillum Lorem ullamco minim nostrud elit officia tempor
+						esse quis. Sunt ad dolore quis aute consequat. Magna
+						exercitation reprehenderit magna aute tempor cupidatat
+						consequat elit dolor adipisicing. Mollit dolor eiusmod
+						sunt ex incididunt cillum quis. Velit duis sit officia
+						eiusmod Lorem aliqua enim laboris do dolor eiusmod. Et
+						mollit incididunt nisi consectetur esse laborum eiusmod
+						pariatur proident Lorem eiusmod et. Culpa deserunt
+						nostrud ad veniam.
+					</ModalBody>
+					<ModalFooter>
+						<HStack w="full" justifyContent="end" spacingX={2}>
+							<Button bg="gray.400" onClick={controls.close}>
+								Cancel
+							</Button>
+							<Button bg="blue.400">Save</Button>
+						</HStack>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
 
 			<pre class="prism-code language-js">
 				<code>{code}</code>
 			</pre>
-		</Layout>
+		</>
 	);
 };
 export default ModalComponent;
