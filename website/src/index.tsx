920d2d40e89371e6b4885bc58a284dcaceb05ecd
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-07 14:37:43 -0400

    Moved provider to index file

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -3,12 +3,15 @@ import { render } from "solid-js/web";
 import "./index.css";
 import App from "./App";
 import { Router } from "solid-app-router";
+import { SimpleProvider } from "simple-ui-solid";
 
 render(
 	() => (
-		<Router>
-			<App />
-		</Router>
+		<SimpleProvider>
+			<Router>
+				<App />
+			</Router>
+		</SimpleProvider>
 	),
 	document.getElementById("root")!
 );
