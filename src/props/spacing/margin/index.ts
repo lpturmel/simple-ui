6d46549531ff1cd904c6f7bf6b84aa15d53f3410
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-09 17:27:31 -0400

    Removed prop variants from margin

--- src/props/spacing/margin/index.ts
+++ src/props/spacing/margin/index.ts
@@ -1,20 +1,13 @@
 import { SimpleRemValues } from "../../Global";
 
 export interface MarginProps {
-	m?: marginValues;
-	my?: marginValues;
-	mx?: marginValues;
-	ml?: marginValues;
-	mt?: marginValues;
-	mr?: marginValues;
-	mb?: marginValues;
 	margin?: marginValues;
 	marginX?: marginValues;
 	marginY?: marginValues;
-	marginL?: marginValues;
-	marginT?: marginValues;
-	marginR?: marginValues;
-	marginB?: marginValues;
+	marginLeft?: marginValues;
+	marginTop?: marginValues;
+	marginRight?: marginValues;
+	marginBottom?: marginValues;
 }
 
 export type marginValues = SimpleRemValues | "auto";
