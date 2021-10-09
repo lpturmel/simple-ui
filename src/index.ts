214b22ffd136f06e2647509f41c0d9d009d832b1
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-08 22:00:33 -0400

    Moved api imports

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ export * from "./components/VStack";
 export * from "./components/Button";
 export * from "./components/Select";
 export * from "./components/Input";
-export * from "./apis/createControls";
 export * from "./components/Modal";
 export * from "./components/Modal/ModalBody";
 export * from "./components/Modal/ModalHeader";
@@ -11,4 +10,5 @@ export * from "./components/Modal/ModalOverlay";
 export * from "./components/Modal/ModalContent";
 export * from "./components/Modal/ModalFooter";
 
+export * from "./apis/createControls";
 export { SimpleProvider } from "./context/SimpleContext";
