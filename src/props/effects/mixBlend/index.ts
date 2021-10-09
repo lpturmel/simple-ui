afee49f1d3f60a05874db2dcd4f51d06abc9cbee
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-08 22:00:06 -0400

    Added mixBlend type declarations

---
+++ src/props/effects/mixBlend/index.ts
@@ -0,0 +1,21 @@
+export interface MixBlendProps {
+	mixBlend?: MixBlendValues;
+}
+
+export type MixBlendValues =
+	| "normal"
+	| "multiply"
+	| "screen"
+	| "overlay"
+	| "darken"
+	| "lighten"
+	| "color-dodge"
+	| "color-burn"
+	| "hard-light"
+	| "soft-light"
+	| "difference"
+	| "exclusion"
+	| "hue"
+	| "saturation"
+	| "color"
+	| "luminosity";
