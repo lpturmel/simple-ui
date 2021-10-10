9bb0f3017b8d30ce35d1617fdea7c0a99aa76fe2
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-09 20:35:27 -0400

    Changed website navbar props

--- website/src/components/Navbar.tsx
+++ website/src/components/Navbar.tsx
@@ -6,12 +6,7 @@ const Navbar: Component = (props) => {
 		<HStack width="full" justifyContent="between" paddingY={2} paddingX={4}>
 			<a href="/"> Simple-ui </a>
 
-			<Input
-				backgroundColor="gray.100"
-				width={64}
-				padding={2}
-				placeholder="Search docs"
-			/>
+			<Input width={64} placeholder="Search docs" />
 			<p> v0.0.17 </p>
 		</HStack>
 	);
