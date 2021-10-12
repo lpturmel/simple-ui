34adc2b203294dec7af5f48f8f721ba302ebb156
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-11 21:39:02 -0400

    Added backdropBlur type declarations

---
+++ src/props/filters/backdropBlur/index.ts
@@ -0,0 +1,13 @@
+export interface BackdropBlurProps {
+	backdropBlur?: BackdropBlurValues;
+}
+
+export type BackdropBlurValues =
+	| "none"
+	| "xs"
+	| "sm"
+	| "md"
+	| "lg"
+	| "xl"
+	| "2xl"
+	| "3xl";
