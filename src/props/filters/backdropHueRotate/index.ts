c2fcf676ff8ce42bca7995164c6918ca6300957e
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-11 22:46:15 -0400

    Added backdropHueRotate type declarations

---
+++ src/props/filters/backdropHueRotate/index.ts
@@ -0,0 +1,16 @@
+export interface BackdropHueRotateProps {
+	backdropHueRotate?: BackdropHueRotateValues;
+}
+
+export type BackdropHueRotateValues =
+	| -180
+	| -90
+	| -60
+	| -30
+	| -15
+	| 0
+	| 15
+	| 30
+	| 60
+	| 90
+	| 180;
