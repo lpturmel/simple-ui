4cfd2ded5f857baf02a8cdb78fe7924a7a69a8e7
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-14 15:47:55 -0400

    Updated backgroundOpacity values

--- src/props/background/opacity/index.ts
+++ src/props/background/opacity/index.ts
@@ -1,20 +1,20 @@
 export interface BackgroundOpacityProps {
-  backgroundOpacity?: BackgroundOpacityValues;
+	backgroundOpacity?: BackgroundOpacityValues;
 }
 
 export type BackgroundOpacityValues =
-  | "bg-opacity-0"
-  | "bg-opacity-5"
-  | "bg-opacity-10"
-  | "bg-opacity-20"
-  | "bg-opacity-25"
-  | "bg-opacity-30"
-  | "bg-opacity-40"
-  | "bg-opacity-50"
-  | "bg-opacity-60"
-  | "bg-opacity-70"
-  | "bg-opacity-75"
-  | "bg-opacity-80"
-  | "bg-opacity-90"
-  | "bg-opacity-95"
-  | "bg-opacity-100";
+	| 0
+	| 5
+	| 10
+	| 20
+	| 25
+	| 30
+	| 40
+	| 50
+	| 60
+	| 70
+	| 75
+	| 80
+	| 90
+	| 95
+	| 100;
