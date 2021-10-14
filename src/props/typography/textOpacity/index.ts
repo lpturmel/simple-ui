9558bf60f907c5776b0d7a42bef2cc431fb12fc1
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-13 21:24:35 -0400

    Added textOpacity type declarations

---
+++ src/props/typography/textOpacity/index.ts
@@ -0,0 +1,20 @@
+export interface TextOpacityProps {
+	textOpacity?: TextOpacityValues;
+}
+
+export type TextOpacityValues =
+	| 0
+	| 5
+	| 10
+	| 20
+	| 25
+	| 30
+	| 40
+	| 50
+	| 60
+	| 70
+	| 75
+	| 80
+	| 90
+	| 95
+	| 100;
