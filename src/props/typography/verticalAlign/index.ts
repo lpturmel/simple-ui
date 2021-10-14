a7aebe7fdb6375823493ea17cec680d8f986ce1b
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-14 01:02:51 -0400

    Added verticalAlign type declarations

---
+++ src/props/typography/verticalAlign/index.ts
@@ -0,0 +1,11 @@
+export interface VerticalAlignProps {
+	verticalAlign?: VerticalAlignValues;
+}
+
+export type VerticalAlignValues =
+	| "baseline"
+	| "top"
+	| "middle"
+	| "bottom"
+	| "text-top"
+	| "text-bottom";
