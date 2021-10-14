db9ee297b6cd5d0ebff1eba2266d109fce2c0b66
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-13 20:23:53 -0400

    Added letterSpacing type declarations

---
+++ src/props/typography/letterSpacing/index.ts
@@ -0,0 +1,11 @@
+export interface LetterSpacingProps {
+	letterSpacing?: LetterSpacingValues;
+}
+
+export type LetterSpacingValues =
+	| "tighter"
+	| "tight"
+	| "normal"
+	| "wide"
+	| "wider"
+	| "widest";
