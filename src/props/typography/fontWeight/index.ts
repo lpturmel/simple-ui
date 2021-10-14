1cafb19c5788ec7b56171d92b801b61324ea90b2
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-13 20:23:10 -0400

    Added fontWeight type declarations

---
+++ src/props/typography/fontWeight/index.ts
@@ -0,0 +1,14 @@
+export interface FontWeightProps {
+	fontWeight?: FontWeightValues;
+}
+
+export type FontWeightValues =
+	| "thin"
+	| "extralight"
+	| "light"
+	| "normal"
+	| "medium"
+	| "semibold"
+	| "bold"
+	| "extrabold"
+	| "black";
