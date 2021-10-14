ad9c04fae356fcbf10f715d3d04306ac712c96ca
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-13 22:40:00 -0400

    Updated fontSize errors

--- src/props/typography/fontSize/index.ts
+++ src/props/typography/fontSize/index.ts
@@ -3,20 +3,16 @@ export interface FontSizeProps {
 }
 
 export type FontSizeValues =
-	| "0.75rem"
-	| "1rem"
-	| "0.875rem"
-	| "1.25rem"
-	| "1.5rem"
-	| "1.125rem"
-	| "1.75rem"
-	| "2rem"
-	| "1.875rem"
-	| "2.25rem"
-	| "2.5rem"
-	| "3rem"
-	| "1"
-	| "3.75rem"
-	| "4.5rem"
-	| "6rem"
-	| "8rem";
+	| "xs"
+	| "sm"
+	| "base"
+	| "lg"
+	| "xl"
+	| "2xl"
+	| "3xl"
+	| "4xl"
+	| "5xl"
+	| "6xl"
+	| "7xl"
+	| "8xl"
+	| "9xl";
