5882159bc1e7f499573bff17ef901aaa0dbfdcfd
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-14 23:57:45 -0400

    Updated HStack story

--- src/components/HStack/HStack.stories.tsx
+++ src/components/HStack/HStack.stories.tsx
@@ -12,14 +12,8 @@ export const Default = () => (
 	<SimpleProvider>
 		<StoryWrapper>
 			<HStack padding={4} spacingX={4}>
-				<Button>Button</Button>
-				<Button
-					_hover={{ backgroundColor: "blue.400" }}
-					paddingX={4}
-					paddingY={2}
-				>
-					Button
-				</Button>
+				<Button color="white">Button</Button>
+				<Button color="white">Button</Button>
 			</HStack>
 		</StoryWrapper>
 	</SimpleProvider>
