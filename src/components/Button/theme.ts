397f97c09337fecfdd77af27b1ab0783b11652a1
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-14 23:57:58 -0400

    Updated Button theme

--- src/components/Button/theme.ts
+++ src/components/Button/theme.ts
@@ -5,21 +5,50 @@ const ButtonOptions: ThemeComponentOptions = {
 		paddingX: 4,
 		paddingY: 2,
 		borderRadius: "md",
-		fontSize: "lg",
+		fontSize: "md",
 		fontWeight: "semibold",
-		color: "white",
-		backgroundColor: "gray.400",
-		_hover: {
-			backgroundColor: "gray.500",
-		},
-		_active: {
-			backgroundColor: "gray.600",
-		},
 		_focus: {
 			ringWidth: 3,
 		},
 		transitionDuration: 200,
 		transition: "colors",
+	},
+	variants: {
+		outlined: {
+			color: "blue.400",
+			backgroundColor: "transparent",
+			borderWidth: 1,
+			borderColor: "blue.400",
+			_hover: {
+				backgroundColor: "whitealpha.100",
+			},
+			_active: {
+				backgroundColor: "whitealpha.200",
+			},
+		},
+		ghost: {
+			backgroundColor: "transparent",
+		},
+	},
+	dark: {
+		color: "white",
+		backgroundColor: "whitealpha.200",
+		_hover: {
+			backgroundColor: "whitealpha.300",
+		},
+		_active: {
+			backgroundColor: "whitealpha.400",
+		},
+	},
+	light: {
+		color: "black",
+		backgroundColor: "gray.200",
+		_hover: {
+			backgroundColor: "gray.300",
+		},
+		_active: {
+			backgroundColor: "gray.400",
+		},
 	},
 };
 
