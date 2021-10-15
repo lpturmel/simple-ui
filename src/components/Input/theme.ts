b42e89ac1833c34fa802c9ecf2ff80d45f8ae252
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-14 23:58:31 -0400

    Updated Input theme

--- src/components/Input/theme.ts
+++ src/components/Input/theme.ts
@@ -6,6 +6,7 @@ const InputOptions: ThemeComponentOptions = {
 		transition: "basic",
 		transitionDuration: 200,
 		transitionTiming: "ease-linear",
+		backgroundColor: "transparent",
 		borderWidth: 1,
 		_focus: {
 			ringWidth: 2,
@@ -18,9 +19,19 @@ const InputOptions: ThemeComponentOptions = {
 		placeholderColor: "gray.400",
 		paddingX: 4,
 		fontSize: "md",
+	},
+	light: {
+		color: "black",
 		borderColor: "gray.200",
 		_hover: {
 			borderColor: "gray.300",
+		},
+	},
+	dark: {
+		color: "white",
+		borderColor: "whitealpha.300",
+		_hover: {
+			borderColor: "whitealpha.500",
 		},
 	},
 };
