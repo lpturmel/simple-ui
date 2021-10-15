a6b9dec64dfe44ce376fde6409a272daf8a30fae
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-14 23:57:18 -0400

    Updated HStack story

--- src/components/VStack/HStack.stories.tsx
+++ src/components/VStack/HStack.stories.tsx
@@ -1,3 +1,5 @@
+import { SimpleProvider } from "../..";
+import StoryWrapper from "../../StoryWrapper";
 import { Button } from "../Button/Button";
 import { VStack } from "./VStack";
 
@@ -7,12 +9,16 @@ export default {
 };
 
 export const Default = () => (
-	<VStack width="2/12" class="max-w-2">
-		<Button paddingX={4} paddingY={2}>
-			VStack Child 1
-		</Button>
-		<Button paddingX={4} paddingY={2}>
-			VStack Child 2
-		</Button>
-	</VStack>
+	<SimpleProvider>
+		<StoryWrapper>
+			<VStack spacingY={4} width="2/12" class="max-w-2">
+				<Button paddingX={4} paddingY={2}>
+					VStack Child 1
+				</Button>
+				<Button paddingX={4} paddingY={2}>
+					VStack Child 2
+				</Button>
+			</VStack>
+		</StoryWrapper>
+	</SimpleProvider>
 );
