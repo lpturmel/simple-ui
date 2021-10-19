c570e01bfc749681d6c4eafcfbf86cd752dc46de
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-18 23:54:32 -0400

    Parser: Added sample component folder and file for tests

---
+++ simple-ui-cli/parser/src/sample/Component.tsx
@@ -0,0 +1,15 @@
+import { Component } from "solid-js";
+import { Button } from "simple-ui-solid";
+
+const MyComponent: Component = (props) => {
+  return (
+    <>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+    </>
+  );
+};
+
+// export default MyComponent;
