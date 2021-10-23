2f7cbc62747d7a6f8a117b6036ee5abf75c75033
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-22 23:16:28 -0400

    Updated ModalHeader theme and definition

--- src/components/Modal/ModalHeader/index.tsx
+++ src/components/Modal/ModalHeader/index.tsx
@@ -1,6 +1,6 @@
 import { Component, createEffect, createSignal, JSX } from "solid-js";
 import MainProps from "../../../props";
-import { mapPropsToCss, mergeProps } from "../../../props/Global";
+import { mergeClasses } from "../../../props/global";
 
 export interface ModalHeaderProps
 	extends MainProps<JSX.HTMLAttributes<HTMLDivElement>> {}
@@ -8,8 +8,7 @@ export const ModalHeader: Component<ModalHeaderProps> = (props) => {
 	const [simpleProps, setSimpleProps] = createSignal<string>("");
 
 	createEffect(() => {
-		const mergedProps = mergeProps("ModalHeader", props);
-		setSimpleProps(mapPropsToCss(mergedProps, true));
+		setSimpleProps(mergeClasses("ModalHeader", props));
 	});
 	return <div class={simpleProps()}>{props.children}</div>;
 };
--- src/components/Modal/ModalHeader/theme.ts
+++ src/components/Modal/ModalHeader/theme.ts
@@ -1,15 +1,18 @@
 import { ThemeComponentOptions } from "../../../theme";
 
 const ModalHeaderOptions: ThemeComponentOptions = {
-	defaultProps: {
-		display: "flex",
-		flexDirection: "row",
-		justifyContent: "between",
-		paddingX: 4,
-		paddingY: 4,
-		fontSize: "xl",
-		fontWeight: "semibold",
+	default: {
+		class: "flex flex-row justify-between px-4 py-4 text-xl font-semibold",
 	},
+	// defaultProps: {
+	// 	display: "flex",
+	// 	flexDirection: "row",
+	// 	justifyContent: "between",
+	// 	paddingX: 4,
+	// 	paddingY: 4,
+	// 	fontSize: "xl",
+	// 	fontWeight: "semibold",
+	// },
 };
 
 export default ModalHeaderOptions;
