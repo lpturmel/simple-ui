e8d5b44423126ef6c9c185a45df0af9db65b2cfd
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-22 23:17:11 -0400

    Updated StoryWrapper.tsx

--- src/StoryWrapper.tsx
+++ src/StoryWrapper.tsx
@@ -1,38 +1,12 @@
 import { Component } from "solid-js";
 import { Button, useColorMode } from ".";
-import { HStack } from ".";
+import { HStack } from "./components/HStack";
 const StoryWrapper: Component = (props) => {
 	const [colorMode, toggleColorMode] = useColorMode();
 
 	return (
-		<HStack
-			position="relative"
-			padding={16}
-			spacingX={8}
-			width="full"
-			height="screen"
-			alignItems="start"
-		>
-			<Button
-				paddingTop={0}
-				paddingBottom={0}
-				paddingRight={0}
-				paddingLeft={0}
-				backgroundColor="transparent"
-				_hover={{
-					backgroundColor: "gray.600",
-					backgroundOpacity: 20,
-				}}
-				_active={{
-					backgroundColor: "gray.700",
-				}}
-				width={10}
-				height={10}
-				onClick={() => toggleColorMode()}
-				position="absolute"
-				top={1}
-				right={1}
-			>
+		<HStack class="relative p-8 space-x-8 w-full h-screen items-start">
+			<Button class="bg-transparent w-10 h-10 absolute top-1 right-1 hover:bg-gray-600 hover:bg-opacity-20 active:bg-gray-700">
 				{colorMode() === "dark" ? "🔅" : "🌑"}
 			</Button>
 			{props.children}
