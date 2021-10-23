4c25714da38131a14d59656f55de41c7158518ac
lpturmel <louisphilippe.turmel@gmail.com>
2021-10-22 23:15:57 -0400

    Updated ModalBody theme and definition

--- src/components/Modal/ModalBody/index.tsx
+++ src/components/Modal/ModalBody/index.tsx
@@ -1,6 +1,6 @@
 import { Component, createEffect, createSignal, JSX } from "solid-js";
 import MainProps from "../../../props";
-import { mapPropsToCss, mergeProps } from "../../../props/Global";
+import { mergeClasses } from "../../../props/global";
 
 export interface ModalBodyProps
 	extends MainProps<JSX.HTMLAttributes<HTMLDivElement>> {}
@@ -8,8 +8,7 @@ export const ModalBody: Component<ModalBodyProps> = (props) => {
 	const [simpleProps, setSimpleProps] = createSignal<string>("");
 
 	createEffect(() => {
-		const mergedProps = mergeProps("ModalBody", props);
-		setSimpleProps(mapPropsToCss(mergedProps, true));
+		setSimpleProps(mergeClasses("ModalBody", props));
 	});
 
 	return <div class={simpleProps()}>{props.children}</div>;
--- src/components/Modal/ModalBody/theme.ts
+++ src/components/Modal/ModalBody/theme.ts
@@ -1,11 +1,14 @@
 import { ThemeComponentOptions } from "../../../theme";
 
 const ModalBodyOptions: ThemeComponentOptions = {
-	defaultProps: {
-		zIndex: 50,
-		paddingX: 4,
-		paddingY: 4,
+	default: {
+		class: "z-50 px-4 py-4",
 	},
+	// defaultProps: {
+	// 	zIndex: 50,
+	// 	paddingX: 4,
+	// 	paddingY: 4,
+	// },
 };
 
 export default ModalBodyOptions;
